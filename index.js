de4e7c09255ee6518222e17c99a6533bd2451df3
sunshine <2331820374@qq.com>
2023-09-12 09:33:39 +0800

    渲染器初步实现

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import {reactive} from './reactive/index.js'
+import { reactive } from './reactive/index.js'
 
 /* 测试使用 */
+import './test/render.text.js' // render 函数使用
 import scheduler from './test/scheduler.js'
 import computed from './test/computed.js'
 import cleanup from './test/cleanup.js'
 import watch from './test/watch.js'
 import effectTest from './test/effectTest.js'
 import reactiveText from './test/reactive.text.js'
+
 
 let obj = {
   name: '阳光',
@@ -18,7 +20,6 @@ let obj = {
 
 
 let proxyObj = reactive(obj)
-
 
 /* 调度器 只会执行一次 */
 // scheduler(proxyObj)
---
+++ test/render.text.js
@@ -0,0 +1,41 @@
+import { renderer } from "../renderer/index.js"
+
+debugger
+const vnode = {
+  tag: 'div',
+  props: {
+    onClick: () => alert('hello')
+  },
+  children: '测试render函数按钮' // 从 click me 改成 click again
+}
+
+// MyComponent 是一个对象
+const MyComponent = {
+  render() {
+    return {
+      tag: 'div',
+      props: {
+        onClick: () => alert('hello')
+      },
+      children: 'click me'
+    }
+  }
+}
+// const vnode = {
+//    tag: MyComponent
+// }
+
+function render() {
+  // 为了效果更加直观，这里没有使用 h 函数，而是直接采用了虚拟 DOM 对象
+  // 下面的代码等价于：
+  // return h('div', { id: 'foo', class: cls })
+  return {
+    tag: 'div',
+    props: {
+      id: 'foo',
+      class: cls
+    }
+  }
+}
+
+renderer(vnode, document.body)
